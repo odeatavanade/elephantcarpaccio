ba1e480ae8790e5e6217a2256963b265ce8e34e1
Olivier Delmotte <odeatavanade@users.noreply.github.com>
2023-10-27 18:12:17 +0200

    implem

--- UnitTests/ElephantCarpaccio/Calculator.cs
+++ UnitTests/ElephantCarpaccio/Calculator.cs
@@ -2,6 +2,19 @@
 {
     public class Calculator
     {
+        static readonly Dictionary<string, decimal> taxes = new Dictionary<string, decimal>{
+            {"ut", 1.0685M},
+            {"nv", 1.080M},
+            {"tx", 1.0625M},
+            {"al", 1.04M},
+            {"ca", 1.0825M},
+        };
 
+        public static decimal GetPrice(int quantity, decimal price, string state)
+        {
+            var sum = quantity * price;
+            var taxe = taxes[state];
+            return Math.Round(sum * taxe, 2);
+        }
     }
 }
--- UnitTests/UnitTests/UnitTest1.cs
+++ UnitTests/UnitTests/UnitTest1.cs
@@ -1,11 +1,17 @@
+using ElephantCarpaccio;
+
 namespace UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow(1, 10, "ut", 10.68)]
+        [DataRow(3, 40, "ut", 128.22)]
+        [DataRow(978, 270.99, "ut", 283182.65)]
+        public void CalculatePrice(int quantity, double price, string state, double expected)
         {
+            Assert.AreEqual((decimal)expected, Calculator.GetPrice(quantity, (decimal)price, state));
         }
     }
 }
